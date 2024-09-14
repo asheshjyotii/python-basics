7cc6ee627bd07358b0f770933d7820d2b6a67941
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2024-09-14 14:41:58 +0530

    created string folder, created concatenation example

---
+++ string/concatenation.py
@@ -0,0 +1,13 @@
+# Concatenation is the joining of two or more things in this case two or more things
+
+string_1 = "i am\t"
+string_2 = "Ashesh Jyoti Majumdar.\t"
+string_3 = "I want to become a developer!"
+
+print(string_1+string_2+string_3)
+
+'''
+Output
+
+i am    Ashesh Jyoti Majumdar.  I want to become a developer!
+'''
