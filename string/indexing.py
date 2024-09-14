ddc07da08cb8c577d4a7d82477eebb58b17a9a2c
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2024-09-14 16:54:20 +0530

    String indexing example added

---
+++ string/indexing.py
@@ -0,0 +1,13 @@
+# letters from a string can be fetched using string indexing
+# indexing starts from 0
+
+string = "hi this is Ram"
+
+# printing the first letter
+print(string[0])
+
+# printing the last letter using -1
+print(string[-1])
+
+# printing the last letter using len function
+print(string[len(string)-1])
