031b6e62d7f8da783c56a69aacd386068d887e12
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2024-09-21 00:33:45 +0530

    slicing

---
+++ string/slicing.py
@@ -0,0 +1,6 @@
+string = "Ashesh Jyoti Majumdar"
+print(len(string))
+
+print("First four letters", string[:4])
+print("Last four letters", string[-4:])
+print("First four using negative indexing", string[-21:-17])
