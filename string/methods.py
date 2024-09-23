1297a2b044e0e48d2362652a0e5c69ef32488fa2
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2024-09-24 00:32:18 +0530

    Various String methods

---
+++ string/methods.py
@@ -0,0 +1,72 @@
+'''
+1. Write a script that converts the following strings to lowercase: "An-
+imals", "Badger", "Honey Bee", "Honeybadger". Print each lowercase
+string on a separate line.
+'''
+string = ["Animals", "Badger", "Honey Bee", "Honeybadger"]
+print("Lower")
+for x in string :
+    x = x.lower() # since string are immutable therefore reassigning
+    print(x+"\n")
+
+'''
+2. Repeat Exercise 1, but convert each string to uppercase instead of
+lowercase.
+'''
+
+string = ["Animals", "Badger", "Honey Bee", "Honeybadger"]
+print("Upper")
+for x in string :
+    x = x.upper()
+    print(x+"\n")
+
+'''
+3. Write a script that removes whitespace from the following strings:
+string1 = " Filet Mignon"
+string2 = "Brisket "
+string3 = " Cheeseburger "
+Print out the strings with the whitespace removed.
+'''
+
+string1 = " Filet Mignon"
+string2 = "Brisket "
+string3 = " Cheeseburger "
+print("removing whitespace...")
+print(string1.lstrip())
+print(string2.rstrip())
+print(string3.strip())
+
+'''
+4. Write a script that prints out the result of .startswith("be") on each
+of the following strings:
+string1 = "Becomes"
+string2 = "becomes"
+string3 = "BEAR"
+string4 = " bEautiful"
+'''
+
+string1 = "Becomes"
+string2 = "becomes"
+string3 = "BEAR"
+string4 = " bEautiful"
+print("The strings starting with 'be' are")
+print(string1+"->",string1.startswith("be"))
+print(string2+"->",string2.startswith("be"))
+print(string3+"->",string3.startswith("be"))
+print(string4+"->",string4.startswith("be"))
+
+'''
+5. Using the same four strings from Exercise 4, write a script that
+uses string methods to alter each string so that .startswith("be")
+returns True for all of them
+'''
+print("checking the spelling without case sensitive")
+string = ["Becomes", "becomes" ,"BEAR"," bEautiful"]
+for index, elements in enumerate(iterable=string,start=1):
+    x = x.strip()
+    x = x.lower()
+    if x.startswith("be"):
+        print(f"string{index} - > True")
+    else:
+        print(f"string{index} - > False")
+
